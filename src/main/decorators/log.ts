25c28996e0f257af6e7540779bb9446cd56faf83
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2022-10-31 10:23:56 -0300

    feat: ensure LogControllerDecorator returns the same result of the controller

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -9,12 +9,6 @@ export class LogControllerDecorator implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const httpResponse = await this.controller.handle(httpRequest)
-    if (httpResponse.statusCode === 500) {
-      return {
-        statusCode: 500,
-        body: 'error'
-      }
-    }
     return httpResponse
   }
 }
