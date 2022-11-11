c1f7fd61d934ab7eb2c10833fec9a6573ed2a999
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2022-11-11 10:29:13 -0300

    refactor: move required fild validation to the ValidationComposite

---
+++ src/presentation/helpers/validators/required-field-validation.ts.ts
@@ -0,0 +1,14 @@
+import { MissingParamError } from '../../errors'
+import { Validation } from './validation'
+
+export class RequiredFieldsValidation implements Validation {
+  constructor (private readonly fieldName: string) {
+    this.fieldName = fieldName
+  }
+
+  validate (input: any): any {
+    if (!input[this.fieldName]) {
+      return new MissingParamError(this.fieldName)
+    }
+  }
+}
