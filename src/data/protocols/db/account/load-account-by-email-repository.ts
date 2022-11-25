b519d23825da72cf7224487188e4c175f7a8542d
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2022-11-25 16:33:00 -0300

    refactor: change build config

--- src/data/protocols/db/account/load-account-by-email-repository.ts
+++ src/data/protocols/db/account/load-account-by-email-repository.ts
@@ -1,4 +1,4 @@
-import { AccountModel } from '../../../domain/models/account'
+import { AccountModel } from '../../../../domain/models/account'
 
 export class LoadAccountByEmailRepository {
   loadByEmail: (email: string) => Promise<AccountModel> | null
