6bc4ed551f357d2cc61b9b9fbd30678523eec4aa
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2022-11-25 16:11:49 -0300

    Revert "refactor: change routes to use filesytem instead of fast-glob"
    This reverts commit cf4f3b0fd0205a0986bdc891192bcad1591b5f52.

--- dist/data/protocols/db/account/load-account-by-email-repository.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.LoadAccountByEmailRepository = void 0;
-var LoadAccountByEmailRepository = /** @class */ (function () {
-    function LoadAccountByEmailRepository() {
-    }
-    return LoadAccountByEmailRepository;
-}());
-exports.LoadAccountByEmailRepository = LoadAccountByEmailRepository;
--- dist/main/config/middlewares.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var index_1 = require("../middlewares/index");
-exports["default"] = (function (app) {
-    app.use(index_1.bodyParser);
-    app.use(index_1.cors);
-    app.use(index_1.contentType);
-});
