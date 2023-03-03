beaaf256ba5dc563fb1c44efc3582eb5e75bc0a9
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2023-03-03 11:02:33 -0300

    refactor: move auth middlewares to separate files

--- src/main/factories/controllers/survey/load-surveys/load-surveys-controller-factory.ts
+++ src/main/factories/controllers/survey/load-surveys/load-surveys-controller-factory.ts
@@ -1,5 +1,5 @@
-import { LoadSurveysController } from './../../../../../presentation/controllers/survey/load-surveys/load-surveys-controller'
-import { makeDbLoadSurveys } from './../../../usecases/survey/load-surveys/db-load-surveys'
+import { LoadSurveysController } from '../../../../../presentation/controllers/survey/load-surveys/load-surveys-controller'
+import { makeDbLoadSurveys } from '../../../usecases/survey/load-surveys/db-load-surveys'
 import { Controller } from '../../../../../presentation/protocols'
 import { makeLogControllerDecorator } from '../../../decorators/log-controller-decorator-factory'
 
--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,14 +1,11 @@
-import { makeAuthMiddleware } from './../factories/middlewares/auth-middleware'
+import { auth } from './../middlewares/auth'
+import { adminAuth } from './../middlewares/admin-auth'
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory'
-import { adaptMiddleware } from '../adapters/express-middleware-adapter'
-import { makeLoadSurveysController } from '../factories/controllers/survey/load-surveys/load-surveys-controller-factory.spec'
+import { makeLoadSurveysController } from '../factories/controllers/survey/load-surveys/load-surveys-controller-factory'
 
 export default (router: Router): void => {
-  const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
-  const auth = adaptMiddleware(makeAuthMiddleware())
-
   router.post('/surveys', adminAuth, adaptRoute(makeAddSurveyController()))
   router.get('/surveys', auth, adaptRoute(makeLoadSurveysController()))
 }
