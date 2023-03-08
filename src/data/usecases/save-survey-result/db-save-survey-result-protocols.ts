4eaae41993abc1dca7062596f64e99f10a2f06a8
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2023-03-08 12:40:14 -0300

    feat: feat ensure DbSaveSurveyResult calls SaveSurveyResultRepository with correct values

---
+++ src/data/usecases/save-survey-result/db-save-survey-result-protocols.ts
@@ -0,0 +1,3 @@
+export * from '../../protocols/db/survey/save-survey-result-repository'
+export * from '../../../domain/models/survey-result'
+export * from '../../../domain/usescases/save-survey-result'
--- src/domain/models/survey-result.ts
+++ src/domain/models/survey-result.ts
@@ -1,7 +1,7 @@
 export type SurveyResultModel = {
   id: string
   surveyId: string
-  accoundId: string
+  accountId: string
   answer: string
   date: Date
 }
