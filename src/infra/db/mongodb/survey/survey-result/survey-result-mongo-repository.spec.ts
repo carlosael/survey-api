32ebf1fcdcf7591ab5bc2ae0917de750bd439f3e
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2023-03-10 09:56:53 -0300

    test: ensure SurveyResultMongoRepository insert new survey result if there is none

---
+++ src/infra/db/mongodb/survey/survey-result/survey-result-mongo-repository.spec.ts
@@ -0,0 +1,74 @@
+import { SurveyModel } from './../../../../../domain/models/survey'
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../../helper/mongo-helper'
+import { SurveyResultMongoRepository } from './survey-result-mongo-repository'
+import { AccountModel } from '@/domain/models/account'
+
+let surveyCollection: Collection
+let surveyResultCollection: Collection
+let accountCollection: Collection
+
+const makeSut = (): SurveyResultMongoRepository => {
+  return new SurveyResultMongoRepository()
+}
+
+const makeSurvey = async (): Promise<SurveyModel> => {
+  const res = await surveyCollection.insertOne({
+    question: 'any_question',
+    answers: [{
+      image: 'any_image',
+      answer: 'any_answer'
+    },
+    {
+      answer: 'another_answer'
+    }
+    ],
+    date: new Date()
+  })
+  return res.ops[0]
+}
+
+const makeFakeAccount = async (): Promise<AccountModel> => {
+  const res = await accountCollection.insertOne({
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'any_password'
+  })
+  return res.ops[0]
+}
+
+describe('Survey Result Mongo Repository', () => {
+  beforeAll(async () => {
+    return await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+    surveyResultCollection = await MongoHelper.getCollection('surveyResults')
+    await surveyResultCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  describe('save()', () => {
+    test('Should add a survey result if its a new register', async () => {
+      const survey = await makeSurvey()
+      const account = await makeFakeAccount()
+      const sut = makeSut()
+      const surveyResult = await sut.save({
+        surveyId: survey.id,
+        accountId: account.id,
+        answer: survey.answers[0].answer,
+        date: new Date()
+      })
+      expect(surveyResult).toBeTruthy()
+      expect(surveyResult.id).toBeTruthy()
+      expect(surveyResult.answer).toBe(survey.answers[0].answer)
+    })
+  })
+})
